1c20cad90209575df183b1693ca709b28c797bfd
Sohan jagadeesh <sjag0009@student.monash.edu>
2023-08-04 13:54:38 +1000

    sending data to backend from the about section

--- starter_repo/Server/server.js
+++ starter_repo/Server/server.js
@@ -28,6 +28,38 @@ connection.query(
     }
   );
 
+const gasEmissionsFactor = 0.006; // kg CO2 per dollar spent
+const electricityEmissionsFactor = 0.005; // kg CO2 per dollar spent
+
+app.post("/api/compareFootprint", (req, res) => {
+  const { gasBill, electricityBill, postcode } = req.body;
+  const userGasFootprint = gasBill * gasEmissionsFactor;
+  const userElectricityFootprint = electricityBill * electricityEmissionsFactor;
+
+  // Query to get neighborhood data from the database
+  const query = 'SELECT avg_gas_bill, avg_electricity_bill FROM neighborhood WHERE postcode = ?';
+  connection.query(query, [postcode], (error, results) => {
+    if (error) throw error;
+
+    // Handle case where postcode is not found
+    if (results.length === 0) {
+      return res.status(404).json({ error: "Postcode not found" });
+    }
+
+    const neighborhoodAvgGasBill = results[0].avg_gas_bill;
+    const neighborhoodAvgElectricityBill = results[0].avg_electricity_bill;
+
+    const neighborhoodGasFootprint = neighborhoodAvgGasBill * gasEmissionsFactor;
+    const neighborhoodElectricityFootprint = neighborhoodAvgElectricityBill * electricityEmissionsFactor;
+
+    res.json({
+      user: [userGasFootprint, userElectricityFootprint],
+      neighborhood: [neighborhoodGasFootprint, neighborhoodElectricityFootprint]
+    });
+  });
+});
+
+
 app.get("/api/home", (req,res) => {
     res.json({ message: "Hello World!"});
 });
